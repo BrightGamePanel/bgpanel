c8b66fa88c7fa93365c6b2089e8c28b3f253c93a
Nikita Rousseau <warhawk3407@gmail.com>
2013-08-25 19:19:47 +0200

    Beta 9: clientprocess fix (thx oz) & AJXP WIP

--- libs/ajxp/bridge.php
+++ libs/ajxp/bridge.php
@@ -27,4 +27,113 @@
  */
 
 
+
+/**
+ *	@Class:		AjaXplorer Bridge Class For BrightGamePanel
+ *	@Version:	1.0
+ *	@Date:		25/08/2013
+ */
+class AJXP_Bridge {
+
+	//------------------------------------------------------------------------------------------------------------+
+	//------------------------------------------------------------------------------------------------------------+
+
+	// VARIABLES
+
+	//------------------------------------------------------------------------------------------------------------+
+	//------------------------------------------------------------------------------------------------------------+
+
+	/**
+	 * BGPanel Workspaces
+	 *
+	 * @var Array
+	 * @access private
+	 */
+	private $bgpWorkspaces	= array();
+
+	/**
+	 * BGPanel User Vars
+	 *
+	 * @var String
+	 * @access private
+	 */
+	private $bgpUser		= '';
+	private $bgpUserRight	= '';
+
+	/**
+	 * AJXP Data Directories
+	 *
+	 * @var String
+	 * @access private
+	 */
+	private $AJXP_DATA_PATH					=	substr( realpath(dirname(__FILE__)), 0, -10 ).'/ajxp/data';
+	private $AJXP_DATA_CONFSERIAL_REPOFILE	=	$this->AJXP_DATA_PATH.'/plugins/conf.serial/repo.ser';
+	private $AJXP_DATA_AUTHSERIAL_DIR		=	$this->AJXP_DATA_PATH.'/plugins/auth.serial';
+	// private $AJXP_DATA_BOOTCONF_FILE		=	$this->AJXP_DATA_PATH.'/plugins/boot.conf/bootstrap.json';
+
+
+	//------------------------------------------------------------------------------------------------------------+
+	//------------------------------------------------------------------------------------------------------------+
+
+	// FUNCTIONS
+
+	//------------------------------------------------------------------------------------------------------------+
+	//------------------------------------------------------------------------------------------------------------+
+
+	// Default Constructor
+	function __construct( $boxes, $servers, $user, $right = 'admin' )
+	{
+		// Test write perms
+		if (
+				(!is_writable($this->AJXP_DATA_PATH)) ||
+				(!is_writable($this->AJXP_DATA_CONFSERIAL_REPOFILE)) ||
+				(!is_writable($this->AJXP_DATA_AUTHSERIAL_DIR))
+			)
+		{
+			trigger_error("AJXP DATA DIRECTORY IS NOT WRITABLE!", E_USER_ERROR);
+		}
+
+		// Test params
+		if ( empty($user) )
+		{
+			trigger_error("NO BGP USER GIVEN !", E_USER_ERROR);
+		}
+
+		$this->bgpWorkspaces	= array_merge($this->bgpWorkspaces, $boxes);
+		$this->bgpWorkspaces	= array_merge($this->bgpWorkspaces, $servers);
+		$this->bgpUser			= $user;
+		$this->bgpUserRight		= $right;
+	}
+
+	//------------------------------------------------------------------------------------------------------------+
+	//------------------------------------------------------------------------------------------------------------+
+
+	/**
+	 * Update AJXP repo serialized file
+	 *
+	 * @param void
+	 * @return bool
+	 * @access public
+	 */
+	public function updateAJXPWorspaces( ) {
+
+		return FALSE;
+	}
+
+	/**
+	 * Update AJXP user
+	 * Add correct repositories to the user with rw perms
+	 *
+	 * @param void
+	 * @return bool
+	 * @access public
+	 */
+	public function updateAJXPUser( ) {
+
+		return FALSE;
+	}
+}
+
+
+
 ?>
