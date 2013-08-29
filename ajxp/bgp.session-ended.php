4467a856342c2e5c4156b5107d66c712894b53f8
Jérémy <jeremy.leboi@gmail.com>
2013-08-29 22:07:27 +0200

    Update bgp.session-ended.php
    delete collapsing a href button in navbar.

--- ajxp/bgp.session-ended.php
+++ ajxp/bgp.session-ended.php
@@ -216,11 +216,6 @@ else if (isClientLoggedIn() == TRUE)
 		<div class="navbar navbar-fixed-top">
 			<div class="navbar-inner">
 				<div class="container-fluid">
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</a>
 					<a class="brand" href="#">Bright Game Panel</a>
 				</div>
 			</div>
